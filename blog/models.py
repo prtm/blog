44d001b7dc63408af5accb44851e91695da297d6
Preetam <android.preetam@gmail.com>
2018-07-30 16:29:28 +0530

    post published manager

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,11 @@ class TimeStampModel(models.Model):
         abstract = True
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status=Post.PUBLISHED)
+
+
 class Post(TimeStampModel):
     DRAFT = 'dr'
     STAGING = 'st'
@@ -30,8 +35,17 @@ class Post(TimeStampModel):
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=now)
+    objects = models.Manager()
+    published = PublishedManager()
     status = models.CharField(
         max_length=2, choices=status_choices, default=DRAFT)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
     def __str__(self):
         return self.title
